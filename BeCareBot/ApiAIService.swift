28b51158ccce933b00ffebbe873f179877cd5cc1
elihanover <eli@Elis-MBP.frontier.com>
2017-08-07 10:31:50 -0400

    0

---
+++ BeCareBot/ApiAIService.swift
@@ -0,0 +1,9 @@
+//
+//  ApiAIService.swift
+//  BeCareBot
+//
+//  Created by Eli Hanover on 6/29/17.
+//  Copyright © 2017 BeCare. All rights reserved.
+//
+
+import Foundation
---
+++ BeCareBot/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  BeCareBot
+//
+//  Created by Eli Hanover on 6/30/17.
+//  Copyright © 2017 BeCare. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: CollectionViewCell {
+
+}
